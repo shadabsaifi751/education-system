26b13cf08041dc418cfcfa2d1f79bfa004252916
mohd shadab saifi <shadabsaifi749@gmail.com>
2023-07-18 22:11:00 +0530

    fixed majur bug

--- src/common/windowResize/index.jsx
+++ src/common/windowResize/index.jsx
@@ -6,25 +6,32 @@ function useWindowSize() {
   });
 
   useEffect(() => {
-    let resizeTimeout;
-
     function handleResize() {
-      clearTimeout(resizeTimeout);
-
-      resizeTimeout = setTimeout(() => {
-        setWindowSize({
-          width: window.innerWidth,
-        });
-      }, 100); // Adjust the debounce delay as needed (e.g., 100ms)
-
+      setWindowSize({
+        width: window.innerWidth,
+      });
     }
 
-    window.addEventListener("resize", handleResize);
+    let animationFrameId;
+
+    function debouncedResize() {
+      // Cancel any previously scheduled animation frame
+      cancelAnimationFrame(animationFrameId);
+      
+      // Schedule a new animation frame to handle resize event
+      animationFrameId = requestAnimationFrame(handleResize);
+    }
+
+    // Attach the debounced resize event listener
+    window.addEventListener("resize", debouncedResize);
+    
+    // Initialize window size on initial mount
     handleResize();
 
     return () => {
-      window.removeEventListener("resize", handleResize);
-      clearTimeout(resizeTimeout);
+      // Cleanup: remove event listener and cancel any scheduled animation frame
+      window.removeEventListener("resize", debouncedResize);
+      cancelAnimationFrame(animationFrameId);
     };
   }, []);
 
@@ -32,3 +39,42 @@ function useWindowSize() {
 }
 
 export default useWindowSize;
+
+
+
+
+
+// import { useState, useEffect } from "react";
+
+// function useWindowSize() {
+//   const [windowSize, setWindowSize] = useState({
+//     width: undefined,
+//   });
+
+//   useEffect(() => {
+//     let resizeTimeout;
+
+//     function handleResize() {
+//       clearTimeout(resizeTimeout);
+
+//       resizeTimeout = setTimeout(() => {
+//         setWindowSize({
+//           width: window.innerWidth,
+//         });
+//       }, 100); // Adjust the debounce delay as needed (e.g., 100ms)
+
+//     }
+
+//     window.addEventListener("resize", handleResize);
+//     handleResize();
+
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//       clearTimeout(resizeTimeout);
+//     };
+//   }, []);
+
+//   return windowSize;
+// }
+
+// export default useWindowSize;
